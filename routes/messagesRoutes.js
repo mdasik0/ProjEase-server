d22cf50254edd1df3b398ada45e9e0cb357cc9aa
mdasik0 <asikthe1st@gmail.com>
2025-01-13 11:50:27 +0600

    created new api to add chat initials to chat-group collection

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("bson");
 const express = require("express");
 // const { ObjectId } = require("mongodb");
 
@@ -6,6 +7,45 @@ const messagesRoutes = (db) => {
 
   //collections
   const messageCollection = db.collection("messages");
+  const chatGroupCollection = db.collection("chat-group");
+  const projectsCollection = db.collection("projects");
+
+  router.post("/chat-group", async (req, res) => {
+    const chatGroupInfo = req.body;
+    const projectId = chatGroupInfo.projectId;
+  
+    try {
+      if (!chatGroupInfo || Object.keys(chatGroupInfo).length === 0) {
+        return res
+          .status(400)
+          .send({ success: false, message: "Request body cannot be empty." });
+      }
+  
+      const response = await chatGroupCollection.insertOne(chatGroupInfo);
+      console.log(response);
+  
+      if(response.acknowledged) {
+        const updateProject = await projectsCollection.updateOne({_id: ObjectId(projectId)}, {
+          $set: {ChatId : response.insertedId}
+        })
+        if(updateProject.modifiedCount > 0) {
+
+          res.status(201).send({
+            success: true,
+            message: "Chat group created successfully.",
+          })
+        } else {
+          res.status(400).send({success: false, message: 'There was an error creating a chat group for this project. Try again later.'})
+        }
+      }
+    } catch (error) {
+      console.error("Error in chat-group route:", error.message);
+      res
+        .status(500)
+        .send({ success: false, message: "Server error. Please try again." });
+    }
+  });
+  
 
   router.get("/messages/:groupId", async (req, res) => {
     const groupId = req.params.groupId;
