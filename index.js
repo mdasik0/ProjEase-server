ec71e9ae088fe0d970058e85b4ea514766ded83d
Md asik <asikthe1st@gmail.com>
2025-06-23 13:50:02 +0600

    feat : health api added for proper uptime onrender

--- index.js
+++ index.js
@@ -9,18 +9,20 @@ const app = express();
 const server = http.createServer(app);
 
 // CORS first
-app.use(cors({
-  origin: [process.env.CORS_ORIGIN_URL, 'http://localhost:5173'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: [process.env.CORS_ORIGIN_URL, "http://localhost:5173"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
 
 // Handle preflight
-app.options('*', cors());
+app.options("*", cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to Projease");
@@ -29,7 +31,7 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     const db = await connectToDB();
-    
+
     // Import routes here
     const userRoutes = require("./routes/userRoutes");
     const taskRoutes = require("./routes/taskRoutes");
@@ -38,16 +40,23 @@ async function run() {
     const invitationRoute = require("./routes/invitationRoutes");
     const messagesRoutes = require("./routes/messagesRoutes");
     const setupSockets = require("./socket");
-    
+
+    app.get("/health", (req, res) => {
+      res.status(200).json({
+        status: "ok",
+        timestamp: new Date().toISOString(),
+      });
+    });
+
     app.use(userRoutes(db));
     app.use(taskRoutes(db));
     app.use(projectRoutes(db));
     app.use(joinProjectRoute(db));
     app.use(invitationRoute(db));
     app.use(messagesRoutes(db));
-    
+
     setupSockets(server, db);
-    
+
     console.log(`server url=http://localhost:5000`);
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
@@ -58,7 +67,7 @@ run().catch(console.error);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
+  console.error("Error:", err.message);
   if (err.message) {
     res.status(500).send(err.message);
   } else {
